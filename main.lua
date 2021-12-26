ec3a43b8b80c6fb66bc97b38cbc3df4fe243acc9
0x721 <gilgamesh15000times@protonmail.com>
2021-12-25 20:27:27 -0800

    t

--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
-function _update():
+objects = {}
+
+function _update()
 
 end
 
-function _draw():
+function _draw()
 
 end
 
 function _init()
+    title_screen()
+end
+
+function title_screen()
 
 end
